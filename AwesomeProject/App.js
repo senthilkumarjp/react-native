31fb95f8a6ddb15cf689af12079d3d3162df3b2d
senthil <senthilkumarjp55@gmail.com>
2022-12-27 04:43:44 -0800

    first

---
+++ AwesomeProject/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txt}>welcome to react native !</Text>
+      <Button title='jkjkhuuk'></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+   
+  },
+  txt:{
+    color:"red",
+    backgroundColor:"black",
+    padding:10
+  }
+});
